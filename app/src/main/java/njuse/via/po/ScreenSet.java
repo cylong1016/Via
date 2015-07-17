f8a7194d57964e16b27b723ab4e0ab4c8a95cc8b
2393147618@qq.com <wzce2393147618>
2015-07-17 11:47:12 +0800

    ScreenSet添加name以区分不同作品

--- app/src/main/java/njuse/via/po/ScreenSet.java
+++ app/src/main/java/njuse/via/po/ScreenSet.java
@@ -9,14 +9,23 @@ import java.util.LinkedList;
 public class ScreenSet implements Serializable {
     private LinkedList<Screen> screenList = new LinkedList<>();
     private int templateID;                    //模板的id，表示使用选中的模板
+    private String workName;                    //作品的名称，用于区分不同作品
+
 
     public  ScreenSet(int templateID){
         this.templateID=templateID;
+        this.workName="default_work";
     }
     public ScreenSet(){}
 
     public int getTemplateID(){
         return templateID;
+    }
+    public void setWorkName(String name){
+        this.workName=name;
+    }
+    public String getWorkName(){
+        return workName;
     }
     public void setTemplateID(int id){
         this.templateID=id;
