8268118cb90935d0a8ab0cbcaefa061c832ada34
ZingSS <zssheng23@gmail.com>
2015-07-21 21:41:44 +0800

    js css

--- web/js/blur.js
+++ web/js/blur.js
@@ -1,57 +1,78 @@
-// $(document).ready(function(){
-// 	var h = $(".images img").height();
-// 	alert(h);
-// 	// $(".sel").
-// })
-
 window.onload=function(){              //not DOM
 
 	// $(".sel").hide();
 
-	//addSel();				// 添加选项
+	var h = $(".images img").height();
+	$(".sel").height(h);
+
+	addSel();				// 添加选项
+	setAnswer();
+}
+
+function setAnswer () {
+	var answer_num = $(".active .sel").children().length;
+	var answer_length = $(".answer").height();
+	// alert(answer_length);
 }
 
 function addSel(){
 
-	// 如果你要写死，这一段就不用写，写在html里面
-	var sel = "<div class='sel'></div>";
-	var answerA = "<div id='A' class='answer'>A. 今天好天气</div>";
-	var answerB = "<div id='B' class='answer'>B. 今天下雨了</div>";
-	$(".images").append(sel);
-	$(".sel").append(answerA);
-	$(".sel").append(answerB);
-	//到这里
+	if (!$(".active").hasClass("hasSel")) {
+		$(".active").click(function(){
+			$.fn.fullpage.moveSectionDown();
+		})
+		return;
+	}
+	else{
+		// 禁止向下滑
+		$.fn.fullpage.setAllowScrolling(!1);
+	}
 
-	// 写死的，用hide和show来显示
-	// $(".sel").show();
-
-	var h = $(".images img").height();
-	$(".sel").height(h);
-
-	$(".answer").click(function(e){
-		var ev=e || event;
-		var id=ev.target.id;//获取鼠标按下对应的对象的id
-		if (id == "A") {
-			$(".sel").remove();
-		}
-		else{
-			$(".section").remove();
-			addGameOver();
-		}
+	$(".hasSel").click(function(){
+		showSelector(".active .sel");
+		jump();
+		$(".hasSel").unbind("click");
 	})
+
+}
+
+function jump(){
+	$(".active .answer").click(function(e){
+			var ev=e || event;  
+			var id=ev.target.id;//获取鼠标按下对应的对象的id   
+			if (id == "right") {
+				$.fn.fullpage.setAllowScrolling(!0);
+				$(".active").removeClass("hasSel");
+				$(".active .sel").animate({opacity:'0.0'},function(){
+					$(".active .sel").remove();
+					$.fn.fullpage.moveSectionDown();
+				});	
+				
+			}
+			else{
+				// $(".section").remove();
+				addGameOver();
+			}
+		})
 }
 
 function addGameOver(){
-	var bg = "<div class='section game_over'></div>";
+	var bg = "<div class='game_over'><div class='reset_form'></div></div>";
 	var over_text = "<h1>Game Over</h1>";
-	var reset = "<div class='reset'>重新开始</div>"
+	var reset = "<div class='reset'>重新开始</div>";
 
-	$("#fullpage").prepend(bg);
-	$(".game_over").prepend(over_text);
-	$(".game_over").append(reset);
+	$("body").prepend(bg);
+	$(".reset_form").prepend(over_text);
+	$(".reset_form").append(reset);
 
 	$(".reset").click(function(){
 		location.reload();
 	})
 
+}
+
+function showSelector(selector){
+	var $e = $(selector);
+	$e.css({"display": "block", "opacity": "0"})
+		.stop().animate({opacity: "1"});
 }
